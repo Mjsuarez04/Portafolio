5f3d58ad2168f616d7dfb1941180d3d726c91db9
Mjsuarez04 <mjsc_1@live.com>
2023-03-17 00:31:06 -0500

    otracorreccion

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -26,7 +26,7 @@ const ProjectItem = ({ project }) => {
         <div className="realtive flex items-center justify-center hover:-translate-y-1 hover:scale-110 duration-300  w-full shadow-xl shadow-gray-100 rounded-xl group hover:bg-gradient-to-l from-gray-200 to-[#001b5e]">
           <img className=" w-full rounded-xl group-hover:opacity-10" src={project.img} alt="" />
           <div className="hidden group-hover:block relative top-[80px] left-[-40%] translate-x-[-50%] translate-y-[-70%] ">
-          <h3 className="text-1xl font-semibold text-white tracking-wider text-center">{project.proyecto}</h3>
+          <h3 className="text-1xl font-semibold text-white tracking-wider text-center">{project.projectName}</h3>
           <p className="pb-2 pt-2 text-white text-center font-semibold">React Js</p>
           <p onClick={handleClick} className="flex items-center justify-center w-[7rem] p-2 rounded-lg bg-white text-gray-700 font-semibold cursor-pointer text-lg">
             More Info</p>
